db4b5a8c32e72c618edaff0b964c84f50089b6e7
Junseo Park <wnstj444@naver.com>
2023-03-03 00:15:58 +0900

    수학, 투포인터

---
+++ Baekjoon/Baek_2018.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek_2018 {
+  public static void main(String[] args) throws IOException{
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		int N=Integer.parseInt(bf.readLine());
+		
+		int first=1;
+		int last=1;
+		int cnt=1;
+		int sum=1;
+		
+		while(last!=N) {
+			if(sum==N) {
+				cnt++;
+				last++;
+				sum+=last;
+			}else if(sum>N) {
+				sum-=first;
+				first++;
+			}else {
+				last++;
+				sum+=last;
+			}
+		}
+		System.out.print(cnt);
+	}
+}
