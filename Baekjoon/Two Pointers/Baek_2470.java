86695591cd8582acd626db4c9b62215a91dd24a4
jspark <jspark@naver.com>
2023-03-28 16:50:06 +0900

    BOJ 두 용액
    정렬, 이분 탐색, 투 포인터

---
+++ Baekjoon/Two Pointers/Baek_2470.java
@@ -0,0 +1,39 @@
+import java.util.*;
+import java.io.*;
+
+public class Baek_2470{
+	public static void main(String[] args) throws IOException{
+		BufferedReader bf=new BufferedReader(new InputStreamReader(System.in));
+		
+		int N=Integer.parseInt(bf.readLine());
+		int[]arr=new int[N];
+		int MIN=Integer.MAX_VALUE;
+		int a=0;
+		int b=0;
+		
+		StringTokenizer stk=new StringTokenizer(bf.readLine());
+		for(int i=0;i<N;i++)
+			arr[i]=Integer.parseInt(stk.nextToken());
+		
+		Arrays.sort(arr);
+	
+		int l=0;
+		int r=arr.length-1;
+		
+		while(l<r) {
+			int num=Math.abs(arr[l]+arr[r]);
+			
+			if(num<MIN) {
+				MIN=num;
+				a=arr[l];
+				b=arr[r];
+			}
+			
+			if(arr[l]+arr[r]>0)
+				r--;
+			else
+				l++;
+		}
+		System.out.print(a+" "+b);
+	}
+}
