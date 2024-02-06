c9b734f82dddf8dd60f39ae9395eed4c85aa43a9
jspark <98972385+JunseoParKK@users.noreply.github.com>
2024-02-06 22:59:46 +0900

    BOJ N과 M (7)

---
+++ Baekjoon/Backtracking/Baek_15656.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class Baek_15656 {
+    static int N, M;
+    static int[] number;
+    static int[] newNumber;
+    static boolean[] visited;
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer stk = new StringTokenizer(bf.readLine());
+
+        N = Integer.parseInt(stk.nextToken());
+        M = Integer.parseInt(stk.nextToken());
+
+        number = new int[N];
+        newNumber = new int[N];
+        visited = new boolean[N];
+
+        stk = new StringTokenizer(bf.readLine());
+        for (int i = 0; i < N; i++) {
+            number[i] = Integer.parseInt(stk.nextToken());
+        }
+        Arrays.sort(number);
+
+        backtracking(0);
+        System.out.print(sb.toString());
+    }
+
+    private static void backtracking(int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(newNumber[i]).append(' ');
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for (int i = 0; i < N; i++) {
+            newNumber[depth] = number[i];
+            backtracking(depth + 1);
+        }
+    }
+}
